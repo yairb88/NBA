c4cc137291945a0ea9b0258abe2fbc4e3f976e8c
yairb88 <yairb88@yahoo.com>
2015-05-29 15:45:33 +0300

    Add WPF Main window for app

--- Nba/Program.cs
+++ Nba/Program.cs
@@ -1,23 +1,33 @@
 ﻿using log4net;
 using log4net.Config;
+using NbaMainWindow;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+
 
 namespace Nba
 {
     class Program
     {
+        
         private static readonly ILog log = LogManager.GetLogger(typeof(Program));
 
-        static void Main(string[] args)
+
+       [STAThread]
+       public static void Main(string[] args)
         {
             Log4NetInitializer.Init();
             log.InfoFormat("Udi Kabudi Industries proud to present");
-
+            //System.Windows.Application app = new Application();
+            Application app = new Application();
+            app.Run(new MainWindow());
         }
+
     }
 }
